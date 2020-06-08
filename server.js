54a141d24cdc4b85c26b1031675558b9955e56e7
partychickenking <partychickenking@gmail.com>
2020-06-08 14:38:11 +0200

    Added Ejs and tried adding an title icon

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
-const express = require('express')
+const express = require('express');
+
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+//Initializing ejs
+app.set('views', 'view')
+app.set('view engine', 'ejs')
+
+app.use('/static', express.static('static'));
+app.get('/', (req, res) => {res.render('index.ejs')})
+
+app.listen(PORT, () => console.log(`Server is currently running on port ${PORT}`));
 
